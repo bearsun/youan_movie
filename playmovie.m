aa7d4c7beb2f6a85e799b011e10c2f9174a8838b
Liwei Sun <sunliwei.cn@outlook.com>
2021-01-15 06:24:28 +0800

    init

---
+++ playmovie.m
@@ -0,0 +1,126 @@
+function playmovie(debug)
+% PLAYMOVIE play movie for subjects at Youan
+% Movie: Almost a Comedy (2019) 1:06 - 21:06
+% Adapted from SimpleMovieDemo
+% Liwei Sun, 1/14/21
+
+ntriggers = 31;
+AssertOpenGL;
+sid = 0;
+moviename = [pwd, '/mclip.mp4'];
+kesc = KbName('Escape');
+
+% MR parameters
+tr = 0;
+pretr = 5 * ntriggers; % wait 5 TRs for BOLD to be stable
+if debug
+    BUFFER = [];
+    fRead = @() ReadFakeTrigger;
+    tr_tmr = timer('TimerFcn', @SetTrigger, 'Period', 2, ...
+        'ExecutionMode', 'fixedDelay', 'Name', 'tr_timer');
+else
+    tbeginning = NaN;
+    trigger = 57; %GE scanner with MR Technology Inc. trigger box
+    IOPort('Closeall');
+    P4 = getport;
+    fRead = @() ReadScanner;
+end
+
+win = Screen('OpenWindow', sid, 0);
+
+% Open movie file:
+movie = Screen('OpenMovie', win, moviename);
+
+% Start playback engine:
+Screen('PlayMovie', movie, 1);
+
+TRWait(pretr);
+% Playback loop: Runs until end of movie or keypress:
+while ~checkkey(kesc)
+    % Wait for next movie frame, retrieve texture handle to it
+    tex = Screen('GetMovieImage', win, movie);
+    
+    % Valid texture returned? A negative value means end of movie reached:
+    if tex<=0
+        % We're done, break out of loop:
+        break;
+    end
+    
+    % Draw the new texture immediately to screen:
+    Screen('DrawTexture', win, tex);
+    
+    % Update display:
+    Screen('Flip', win);
+    
+    % Release texture:
+    Screen('Close', tex);
+end
+
+% Stop playback:
+Screen('PlayMovie', movie, 0);
+
+% Close movie:
+Screen('CloseMovie', movie);
+
+% Close Screen, we're done:
+sca;
+
+if debug
+    StopTimer;
+else
+    IOPort('Closeall');
+end
+sca;
+disp(ncor/(nblocks*ntpb));
+    function [data, when] = ReadScanner
+        [data, when] = IOPort('Read', P4);
+        
+        if ~isempty(data)
+            fprintf('data: %d\n', data);
+            tr = tr + sum(data == trigger);
+            if tr == 1
+                tbeginning = when;
+            end
+            fprintf('%d\t %d\n', when-tbeginning, tr);
+        end
+    end
+
+    function TRWait(t)
+        while t > tr
+            fRead();
+            WaitSecs(.01);
+        end
+    end
+
+    function [data, when] = ReadFakeTrigger
+        data = BUFFER;
+        BUFFER = [];
+%         [~, ~, kDown] = KbCheck;
+%         b = logical(kDown(BUTTONS));
+%         BUFFER = [BUFFER CODES(b)];
+        when = GetSecs;
+    end
+
+    function SetTrigger(varargin)
+        tr = tr + 1;
+        fprintf('TR TRIGGER %d\n', tr);
+        BUFFER = [BUFFER 53];
+    end
+
+    function StopTimer
+        if isobject(tr_tmr) && isvalid(tr_tmr)
+            if strcmpi(tr_tmr.Running, 'on')
+                stop(tr_tmr);
+            end
+            delete(tr_tmr);
+        end
+    end
+
+    function b = checkkey(kesc)
+        [keyIsDown, ~, keyCode] = KbCheck();
+        b = 0;
+        if keyIsDown && find(keyCode) == kesc
+            b = 1;
+        end
+    end
+end
